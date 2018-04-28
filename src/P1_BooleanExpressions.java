b29435fe99ef7aeb1f2ad0dd1e9dc371e9b0846a
dstonemary <37548808+dstonemary@users.noreply.github.com>
2018-04-28 22:30:58 +0530

    Week 6 : Conditional Expression

--- src/P1_BooleanExpressions.java
+++ src/P1_BooleanExpressions.java
@@ -1,36 +1,40 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+  	ISYS 320
+  	Name(s): Derek Stone
+  	Date:    April-27-2018
 */
 
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
+ 1. true
+ 2. true
+ 3. false
+ 4. true
  
- 5.
- 6.
- 7.
- 8.
+ 5. true
+ 6. false
+ 7. true
+ 8. true
    
  */
 
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
+ 1. true
+ 2. true
+ 3. false
+ 4. true
  
- 5.
- 6.
- 7.
- 8.
+ 5. true
+ 6. false
+ 7. true
+ 8. true
+All were correct
+I learned about the output of combination of true and false with operator && (and) and || (or).
+Expression with && operator result will be true if both operands are true other than that result will get false.
+Expression with || operator result will be false if both operands are false other than that result will get true.
 
  */
 public class P1_BooleanExpressions {
@@ -41,7 +45,14 @@ public class P1_BooleanExpressions {
 		int z = 32; 
 		boolean b = false; 
 		
-		System.out.println("REPLACE THIS STRING WITH YOUR BOOLEAN EXPRESSION");
+		System.out.println(!b);
+		System.out.println(b || true);
+		System.out.println((x > y) && (y > z) );
+		System.out.println((x == y) || (x <= z));
+		System.out.println(!(x % 2 == 0)); 
+		System.out.println(b && !b);
+		System.out.println(b || !b);
+		System.out.println((x < y) == b);
 	}
 
 }
--- src/P3_Consecutive.java
+++ src/P3_Consecutive.java
@@ -1,7 +1,7 @@
 /*
-	ISYS 320
-	Name(s):
-	Date: 
+  	ISYS 320
+  	Name(s): Derek Stone
+  	Date:    April-27-2018
 */
 
 import java.util.Scanner;
@@ -21,18 +21,34 @@ public class P3_Consecutive {
 			int num2 = console.nextInt();
 			System.out.print("Enter value 3: ");
 			int num3 = console.nextInt();
-		
-			System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
+			if(areConsecutiveNumbers(num1, num2, num3))			
+				System.out.printf( "a: %d, b: %d, c: %d -> They are consecutive.", num1, num2, num3);
+			else
+				System.out.printf( "a: %d, b: %d, c: %d -> They are not consecutive.", num1, num2, num3);
 			
-
-			
-			System.out.print("Would you like to do it again (enter 'n' to quit)? ");
+			System.out.print("\nWould you like to do it again (enter 'n' to quit)? ");
 			doItAgainResponse = console.next();
 		} while( !doItAgainResponse.equalsIgnoreCase("n"));
 		
 		System.out.println("Thanks!");
 	}
 	
-
+	public static boolean areConsecutiveNumbers(int num1, int num2, int num3){
+		boolean isConsecutive = false;
+		if((num1 + num2 + num3) == 0){
+			if((num1 == 0 || num2 == 0 || num3 ==0) && (num1 == 1 || num2 == 1 || num3 ==1)){
+				isConsecutive = true;
+			}
+		} else {
+			int total = (num1 + num2 + num3);
+			int avg = total / 3;
+			if((avg == num1 && Math.abs(num1-num2) == 1 &&  Math.abs(num1-num3) == 1) ||
+					(avg == num2 && Math.abs(num2-num1) == 1 &&  Math.abs(num2-num3) == 1) ||
+						(avg == num3 && Math.abs(num3-num2) == 1 &&  Math.abs(num3-num2) == 1)){
+				isConsecutive = true;
+			}
+		}
+		return isConsecutive;
+	}
 
 }
